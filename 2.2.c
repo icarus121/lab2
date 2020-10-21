d1bdc518d60d9900e7758420b1aa18586239483b
atikah <2020974753@isiswa.uitm.edu.my>
2020-10-21 18:05:21 +0800

    answer 2.2

--- 2.2.c
+++ 2.2.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>	/*needed to define exit*/
 #include <unistd.h>	/*needed for fork() and getpid()*/
 #include <stdio.h>	/*needed for printf()*/
-#include <sys/wait.h>
+
 
 int main(int argrc, char **argv)
 {
@@ -9,16 +9,16 @@ int main(int argrc, char **argv)
 
 	switch (pid = fork())
 	{
-		case0:	/*a fork return 0 to the child*/
+		case 0:	/*a fork return 0 to the child*/
 			printf("I am the child process:pid=%d\n", getpid());
 			break;
 
 		default:	/*a fork returns a pid to the parent*/
-			wait(NULL);
+			
 			printf("I am the parent process: pid=%d. child pid=%d\n", getpid(), pid);
 			break;
 
-		case-1:	/*something went wrong*/
+		case -1:	/*something went wrong*/
 			perror("fork");
 			exit(1);
 	}
